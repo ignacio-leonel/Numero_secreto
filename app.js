7814a5c21cc37687d74eb9bdf5c0f4330fd138ab
Ignacio Leonel <120539319+ignacio-leonel@users.noreply.github.com>
2024-07-02 15:40:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -48,8 +48,12 @@ function verificarIntento(){
         else{
             asignarTextoAElemento('p', 'El número es mayor');
         }
+
         intentos ++;
+
         limpiarCaja();
+
+        
     }
     
     
@@ -68,53 +72,10 @@ function condicionesIniciales(){
     
 }
 
-function perdedor(){
-    if (intentos =4 ){
-            asignarTextoAElemento('p','Llegaste al número máximo de intentos. Juega otra vez!');
-    condicionesIniciales()
-}
-}
-    
+
 function reiniciarJuego(){
     limpiarCaja();   
     condicionesIniciales();
     
-
-    
+         
 }
-/*
-function ingresarNombre(){
-    mostrar nombre por pantalla
-    variable nombre para usar en varios lados
-
-}
-
-function nivelDificultad(){
-    let nivelSeleccionado= 1;
-    
-    if (nivelSeleccionado=1){
-    numeroMaximo=10 ;
-    cantidadDeIntentos=5;
-    }
-    if(nivelSeleccionado=2){
-    numeroMaximo=10;
-    cantidadDeIntentos=3;
-    }
-    else{
-    numeroMaximo=15;
-    cantidadDeIntentos=3;
-    }
-
-
-    MOSTRAR EL PUNTAJE Y LA CANTIDAD DE INTENTOS RESTANTES.
-    SUMAR PUNTOS SEGUN VAN JUGANDO HACER UNA TABLA DONDE SUMEN PUNTOS CADA INTENTO VAN SUMANDO
-    MENOS PUNTOS GANAR EN NIVEL MAS ALTO SUMA MAS PUNTOS
-    DESACTIVAR EL BOTON INTENTAR CUANDO TERMINAN LOS INTENTOS
-    MOSTRAR MENSAJE GANADOR O PERDEDOR CON EL NOMBRE DEL JUGADOR
-    HACER OTRA PAGINA PRINCIPAL PARA EL JUEGO O VER LA LISTA DE PUNTAJES.
-    GUARDAR UNA LISTA DE JUGADORES ORDENADOS POR PUNTAJES CON NOMBRE Y PUNTOS
-    HACER PAGINA DE VISITANTES PARA QUE DEJEN UNA RESEÑA SOBRE EL JUEGO,
-}
-
-*/
-condicionesIniciales();
